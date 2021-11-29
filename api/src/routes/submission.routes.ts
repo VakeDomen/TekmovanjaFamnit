13a874f018f29d3a2ff637616e386d0b2e3ca367
VakeDomen <vake.domen94@gmail.com>
2021-11-29 11:09:55 +0000

    typo fix

--- api/src/routes/submission.routes.ts
+++ api/src/routes/submission.routes.ts
@@ -17,7 +17,7 @@ router.get("//submission", isValidAuthToken, async (req: express.Request, resp: 
 
 router.get("//submission/:id", isValidAuthToken, async (req: express.Request, resp: express.Response) => {
     if (!req.params['id']) {
-        new SuccessResponse(404, 'No ticks found!').send(resp);
+        new SuccessResponse(404, 'No entries found!').send(resp);
     }
     const data = await fetch(conf.tables.submissions, new Submission({id: req.params['id']}));
     new SuccessResponse().setData(data).send(resp);
