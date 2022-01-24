79ef99303ec95ee43cb48ce065fc39c48c44b669
VakeDomen <vake.domen94@gmail.com>
2022-01-24 08:15:43 +0000

    match data removal

--- cli/main.php
+++ cli/main.php
@@ -109,6 +109,9 @@ foreach ($players as $id => $player) {
     }
 }
 
+//cleanup
+rmdir_recursive("./matches");
+
 
 class Planet
 {
@@ -219,4 +222,12 @@ function colorLog($str, $type = 'i')
             # code...
             break;
     }
+}
+function rmdir_recursive($dir) {
+    foreach(scandir($dir) as $file) {
+        if ('.' === $file || '..' === $file) continue;
+        if (is_dir("$dir/$file")) rmdir_recursive("$dir/$file");
+        else unlink("$dir/$file");
+    }
+    rmdir($dir);
 }
